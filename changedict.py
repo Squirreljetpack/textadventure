8288615ea29532db7405beb5b4829c4c9628dbd7
Squirreljetpack <squirreljetpack@qq.com>
2017-05-25 17:45:26 +0800

    dictionary
    dictionary

---
+++ changedict.py
@@ -0,0 +1,85 @@
+#nouns
+n_berry=['berries','berry']
+n_environment=['around','environment','everything','everywhere','stuff','forest','area','region', 'zone', 'sector','habitat', 'territory', 'domain', 'surroundings', 'conditions', 'environs', 'circumstances',' situation', 'setting','background', 'backdrop', 'scene',
+'scenario', 'location','clues','hints','signs']
+n_boots=['boot','boots','shoe','shoes','footwear']
+n_grass=['grass', 'greenery', 'green', 'sod','field']
+n_ground=['floor','earth','soil','dirt','terrain']
+n_sky=['sky','skies','clouds','up','sun','moon','stars']
+n_backpack=['backpack','pack','bag']
+n_north=['north','forward','forwards']
+n_south=['south','backward','backwards']
+n_east=['east','right']
+n_west=['west','left']
+n_northeast=['northeast']
+n_northwest=['northwest']
+n_southeast=['southeast']
+n_southwest=['southwest']
+it_it='boots'
+
+nouns=['sun','specialnoun','it','oil','compass','oil','porridge']\
++n_berry\
++n_environment\
++n_boots\
++n_grass\
++n_ground\
++n_sky\
++n_backpack\
++n_north\
++n_south\
++n_east\
++n_west\
++n_northeast\
++n_northwest\
++n_southeast\
++n_southwest
+
+
+#verbs
+v_look=['explore','investigate','look','inspect','check','peek','examine','observe','view','probe','see', 'discern', 'perceive', 'glimpse','distinguish', 'identify', 'recognize', 'detect','understand','analyse','know','comprehend','scan'
+,'apprehend']
+v_smell=['smell','sniff','whiff','scent','nose','breathe','inhale','snuff','inbreathe']
+v_touch=['feel','grope','touch', 'stroke', 'caress', 'fondle','prod', 'finger', 'thumb','poke',]
+
+v_open=['open','unfasten', 'unlatch', 'unlock', 'unbolt', 'unbar','unwrap', 'undo', 'untie', 'unseal','broach']
+
+v_harm=['harm','injure','hurt','wound','maltreat','mistreat','misuse','abuse','molest','pain','suffering', 'violence', 'damage','impair', 'deface', 'defile', 'blemish', 'tarnish', 'taint','assault']
+v_punch=['strike', 'jab','hit','slap','smack', 'cuff', 'punch', 'beat', 'thrash', 'thump', 'batter', 'belabour', 'drub', 'hook', 'pound', 'smash', 'slam', 'welt', 'pummel', 'hammer',
+'bang','knock', 'swat', 'whip', 'flog', 'cane', 'whack', 'wallop', 'bash','biff', 'bop', 'clout', 'clobber', 'sock','wham', 'crown', 'belt', 'whomp','thwack','boff','bust','whale','spank']
+v_kick=['kick', 'boot', 'punt']
+v_destroy=['destroy','demolish', 'break', 'wreck', 'ruin', 'waste','shatter','blow', 'dismantle', 'fell', 'spoil','end','devastate','kill','terminate','exterminate','slaughter','annihilate', 'wipe', 'obliterate','eliminate','eradicate',
+'liquidate', 'extinguish', 'finish','erase', 'massacre', 'butcher','extirpate','crush','pulverize','demolish','murder']
+
+v_get=['get','wear','keep','equip','retain','accumulate','save','gather','store','garner','hoard','reserve','stash','grab','extract','pick','choose','select','obtain','acquire','gain','procure','take','possess',
+'harvest','collect','pluck','pull','reap',]
+v_hold=['clasp', 'hold','clutch','grasp','grip','clench','cling','carry','bear','hang','embrace', 'hug', 'cradle', 'fold', 'enfold', 'envelop','sustain','support','brace',]
+v_capture=[]
+v_steal=[]
+v_buy=[]
+
+v_go=['go','walk','proceed','move','travel','journey','advance']
+v_run=['run','sprint','dash']
+
+v_remove=['remove','dismiss','discharge','rid', 'evict', 'eject', 'expel', 'oust', 'purge', 'abolish']
+
+v_no=['fuck','rape','****','***','*****','*****','shit','crap']
+
+verbs=['give','shake','specialverb','wear','use','equip']\
++v_look\
++v_smell\
++v_touch\
++v_open\
++v_harm\
++v_punch\
++v_kick\
++v_destroy\
++v_touch\
++v_get\
++v_hold\
++v_capture\
++v_steal\
++v_buy\
++v_go\
++v_run\
++v_remove\
++v_no
